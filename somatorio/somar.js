8e99c47ef1cb119ca0d656ffb8a02c44be42ead0
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-08-30 23:15:01 +0000

    finalizada pag de sorteio

--- somatorio/somar.js
+++ somatorio/somar.js
@@ -1,8 +1,11 @@
 const btnadd = document.getElementById('add')
 const sum = document.getElementById('sum')
 const number = document.getElementById('number')
+const inputMedia = document.getElementById('media')
+const inputNumdig = document.getElementById('numdig')
+const btnClear = document.getElementById('clear')
 
-btnadd.onclick =()=>{
+btnadd.onclick = ()=>{
     if (isNaN(parseInt(number.value))){
         alert("Valor inválido!")
         return;
@@ -12,9 +15,20 @@ btnadd.onclick =()=>{
         alert('informe apenas valores positivos')
         return;
     }
-    soma = parseInt(sum.value) + parseInt(number.value)
 
+    let numdigitados = parseInt(inputNumdig.value) + 1
+    let soma = parseInt(sum.value) + parseInt(number.value)
+    let media = soma/numdigitados
+
+    inputNumdig.value = numdigitados
+    inputMedia.value = media
     sum.value = soma
     number.value = null
     number.focus();
+}
+
+btnClear.onclick = ()=>{
+    sum.value = 0
+    inputMedia.value = 0
+    inputNumdig.value = 0
 }
---
+++ sorteio/sorteio.js
@@ -0,0 +1,16 @@
+const menorvalor = document.getElementById('menorvalor')
+const maiorvalor = document.getElementById('maiorvalor')
+const btnsorteio = document.getElementById('sorteio')
+const numSorteado = document.getElementById('numSorteado')
+
+btnsorteio.onclick = ()=>{
+
+    if (parseInt(maiorvalor.value) < parseInt(menorvalor.value)){
+        alert('O segundo valor não pode ser menor que o primeiro')
+        return;
+    }   
+    let sorteadoNum = Math.trunc((Math.random() * (parseInt(maiorvalor.value) - parseInt(menorvalor.value) +1) + parseInt(menorvalor.value)))
+    console.log(sorteadoNum);
+    
+    numSorteado.value = sorteadoNum
+}
