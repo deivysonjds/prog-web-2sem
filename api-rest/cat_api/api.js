094591b9ef4a2c56ecadcb794384ab496f315d8f
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-10-13 00:38:12 +0000

    atv fetch completa 1

---
+++ api-rest/cat_api/api.js
@@ -0,0 +1,67 @@
+const btnGerar = document.getElementById('fetchCats')
+
+API_KEY = "live_UUnX6fiC0KoQLuk1s3OvqIwyzUt3jxahTwqYJjd2L2TPMVvlh5hKas32TnXjCFjd"
+URL_BASE = "https://api.thecatapi.com/v1/images/search?limit=10"
+
+const header = {
+    "x-api-key": API_KEY
+}
+
+// const requestOptions = {
+//     method: 'GET',
+//     headers: header
+// }
+
+btnGerar.onclick = ()=>{
+    const divContent = document.getElementById('content')
+    divContent.innerHTML = ''
+    const contentMain = document.getElementById('main-content')
+
+    fetch(URL_BASE, header).then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        let imagesData = data;
+
+        imagesData.map((imageUrl)=>{
+            let divImg = document.createElement('div')
+            divImg.classList.add('img-content')
+            
+            let img = document.createElement('img')
+            img.src = imageUrl.url
+
+            divImg.append(img)
+            divContent.append(divImg)
+        })
+    })
+    
+    const divVerMais = document.createElement('div')
+    divVerMais.id = 'div-more'
+    const btnVerMais = document.createElement('button')
+    divVerMais.classList.add('btn-more')
+    btnVerMais.innerHTML = 'Ver mais'
+
+    btnVerMais.onclick = ()=>{
+        const divContent = document.getElementById('content')
+    
+        fetch(URL_BASE, header).then((response)=>{
+            return response.json()
+        }).then((data)=>{
+            let imagesData = data;
+    
+            imagesData.map((imageUrl)=>{
+                let divImg = document.createElement('div')
+                divImg.classList.add('img-content')
+                
+                let img = document.createElement('img')
+                img.src = imageUrl.url
+    
+                divImg.append(img)
+                divContent.append(divImg)
+            })
+        })
+    }
+
+    divVerMais.append(btnVerMais)
+    contentMain.append(divVerMais)
+}
+
